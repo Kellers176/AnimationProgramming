9c3ef47916a73a7f9ab6fb44ad82f65be73d2578
Kelly <Kelly@localhost>
2019-09-19 18:52:48 -0400

    added other files?

---
+++ Project1/Assets/Scripts/TextHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class TextHandler : MonoBehaviour
+{
+    string text = " ";
+    string line = " ";
+    string[] types;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StreamReader reader = new StreamReader("Assets/TxtFiles/GraphInfo.txt");
+        types = new string[100];
+        int j = 0;
+        while(text != null)
+        {
+            text = reader.ReadLine();
+            //Debug.Log(text);
+            if(text[0] == '@')
+            {
+                for(int i = 1; i < text.Length; i++)
+                {
+                    line += text[i];
+                }
+                types[j] = line;
+                Debug.Log(types[j]);
+                line = " ";
+            }
+            j++;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
