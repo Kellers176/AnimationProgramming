8cf3ba6a9c01b81b2bc073a6948896f0aaf0c154
Kelly <Kelly@localhost>
2019-09-21 16:08:43 -0400

    Got it reading in the object translations

--- Project1/Assets/Scripts/ParseFile.cs
+++ Project1/Assets/Scripts/ParseFile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ParseFile : MonoBehaviour
 {
@@ -17,29 +18,72 @@ public class ParseFile : MonoBehaviour
     ObjectInformation holder;
 
     ObjectInformation[,] ObjectHolder;
+    string text = " ";
+    string line = " ";
 
-
-
+    bool channelNameBool;
     // Start is called before the first frame update
     void Start()
     {
+        //this is to make sure that the struct is working
+        StreamReader reader = new StreamReader("Assets/TxtFiles/pSphere1_KeyframeOutput.txt");
         holder.channelName = "No";
         holder.value = 1;
         holder.keyFrame = 0;
         ColumnLength = 10;
         RowHeight = 10;
         ObjectHolder = new ObjectInformation[ColumnLength, RowHeight];
+        string tempS = "";
+        ObjectInformation temp;
+        temp.channelName = "";
+        temp.value = 0;
+        temp.keyFrame = 0;
 
-        for(int i = 0; i < ColumnLength; i++)
+
+        while (text != null)
         {
-            ObjectHolder[i, 0] = holder;
-            for (int j = 0; j < RowHeight; j++)
+            text = reader.ReadLine().Trim();
+
+
+            if (text[0] == '#')
+                Debug.Log("comment");
+            else
             {
-                ObjectHolder[i, j] = holder;
-                Debug.Log("Channel Name: " + ObjectHolder[i, 0].channelName);
-                Debug.Log("Value: " + ObjectHolder[i, 1].value);
-                Debug.Log("Keyframe: " + ObjectHolder[i, 2].keyFrame);
+                //for every char in the line
+                for (int i = 0; i < text.Length; i++)
+                {
+                    temp.channelName = "";
+                    temp.value = 0;
+                    temp.keyFrame = 0;
+                    if (text[i] == 'p')
+                    {
+                        channelNameBool = true;
+                    }
+                    if(channelNameBool == true)
+                    {
+                        tempS += text[i];
+                    }
+                }
+                //set the channel name from the temp string
+                temp.channelName = tempS;
+                channelNameBool = false;
+                tempS = "";
+
+                //read out the value for each channel name
+                for (int i = 0; i < ColumnLength; i++)
+                {
+                    for (int j = 0; j < RowHeight; j++)
+                    {
+                        ObjectHolder[i, j] = temp;
+                        Debug.Log("Channel Name: " + ObjectHolder[i, 0].channelName);
+                        //Debug.Log("Value: " + ObjectHolder[i, 1].value);
+                        //Debug.Log("Keyframe: " + ObjectHolder[i, 2].keyFrame);
+                    }
+                }
             }
+            text = " ";
+            
+
         }
     }
 
