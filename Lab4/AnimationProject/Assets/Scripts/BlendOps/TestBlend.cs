0c7de5db4ca563df0c9da52df8d43e10e8101a51
Kelly <Kelly@localhost>
2019-10-24 19:11:56 -0400

    Adding in project with notes from class as well as completed first part?

---
+++ Lab4/AnimationProject/Assets/Scripts/BlendOps/TestBlend.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlend : MonoBehaviour
+{
+
+    protected Transform pose_result;
+
+    public bool usingQuaternionRotation = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pose_result = this.gameObject.transform;
+    }
+
+ 
+}
---
+++ Lab4/AnimationProject/Assets/Scripts/BlendOps/TestBlendLerp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBlendLerp : TestBlend
+{
+
+    public Transform pose_0 = null, pose_1 = null;
+
+    [Range(0.0f, 1.0f)]
+    public float parameter = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //translation: literal linear interpolaton
+        pose_result.localPosition = Vector3.Lerp(pose_0.localPosition, pose_1.localPosition, parameter);
+
+        //scale: literal linear interpolation
+        pose_result.localScale = Vector3.Lerp(pose_0.localScale, pose_1.localScale, parameter);
+
+
+        //rotation: SLERP (or NLERP) if using quaternions otherwise regular literal linaer interpolation
+        if(usingQuaternionRotation)
+        {
+            pose_result.localRotation = Quaternion.Slerp(pose_0.localRotation, pose_1.localRotation, parameter);
+        }
+        else
+        {
+            pose_result.localEulerAngles = Vector3.Lerp(pose_0.localEulerAngles, pose_1.localEulerAngles, parameter);
+        }
+    }
+}
