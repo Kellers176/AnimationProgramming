e956dae679cf88378296a26f69a6c4d65669fd00
Kelly Herstine <Kelly Herstine@localhost>
2019-11-29 12:46:38 -0500

    Got project 4 started, pressing W and D either slows the animation or speeds it up

---
+++ Project4/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator anim;
+    public float MoveSpeed;
+    float move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            move += 0.5f;
+            Debug.Log(move);
+            anim.SetFloat("Speed", move);
+            //anim.SetFloat("Direction", turn);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            move -= 0.5f;
+            Debug.Log(move);
+            anim.SetFloat("Speed", move);
+            //anim.SetFloat("Direction", turn);
+        }
+    }
+}
