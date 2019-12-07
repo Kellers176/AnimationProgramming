08a2e518cbe20d18d1ca46310d2475cce87f298d
Kelly Herstine <Kelly Herstine@localhost>
2019-12-06 23:36:38 -0500

    IK is starting to work!!!!

--- AnimProgFinal/Assets/Scripts/IkSolver.cs
+++ AnimProgFinal/Assets/Scripts/IkSolver.cs
@@ -11,6 +11,14 @@ public class IkSolver : MonoBehaviour
     //Constraint locator changes with the player movement
     public Transform poleVector;
 
+    public Transform tochange;
+
+    public GameObject bone1;
+    public GameObject bone2;
+
+    float length1;
+    float length2;
+
     float chainLength;
     float effectorDistance;
 
@@ -29,14 +37,16 @@ public class IkSolver : MonoBehaviour
     Vector3 normalizedDisplacement;   //d carrot
     Vector3 normalizedNPlane;     //n-carrot
 
-    //values
-    float distanceAlongBase;
-    float heightOfTriangle;
-
     //Heron's formula
-    Vector3 baseLength;
-    Vector3 a;
-    Vector3 height;
+    float baseLength;
+    float area;
+    float height;
+
+    //pythagorean theroem
+    float distance;
+
+    //final postion
+    Vector3 finalPosition;
 
     Vector3 CalculateDisplacement(Vector3 left, Vector3 right)
     {
@@ -53,12 +63,31 @@ public class IkSolver : MonoBehaviour
         return temp;
     }
 
+
+    //this is causing errors
     Vector3 FindNormalVec(Vector3 left)
     {
-        //
         Vector3 temp;
         temp = left / Vector3.Magnitude(left);
         return temp;
+    }
+
+    void HeronsFormula()
+    {
+        //get the base length 
+        baseLength = effectorDisplacement.magnitude;
+        //calculate s
+        float s = 0.5f * (baseLength + length1 + length2);
+        //get the sqrt for the area
+        area = Mathf.Sqrt(s * (s - baseLength) * (s - length1) * (s - length2));
+        //calculate the height
+        height = (2 * area) / baseLength;
+
+    }
+    float PythagoreanTheorem(float lhs, float rhs)
+    {
+        float D = Mathf.Sqrt((lhs * lhs) - (rhs * rhs));
+        return D;
     }
 
 
@@ -66,15 +95,17 @@ public class IkSolver : MonoBehaviour
     void Start()
     {
         //this needs to be fixed
-        Transform[] allChildren = this.GetComponentsInChildren<Transform>();
-        foreach (Transform child in allChildren)
-        {
-            if (child.gameObject.transform.GetComponent<BoxCollider>())
-                //chainLength += Vector3.Distance(child.gameObject.transform.position, child.gameObject.transform.position);
-                chainLength += child.gameObject.transform.localScale.y;
-        }
-        
+        //        Transform[] allChildren = this.GetComponentsInChildren<Transform>();
+        //        foreach (Transform child in allChildren)
+        //        {
+        //            if (child.gameObject.transform.GetComponent<BoxCollider>())
+        //                //chainLength += Vector3.Distance(child.gameObject.transform.position, child.gameObject.transform.position);
+        //                chainLength += child.gameObject.transform.localScale.y;
+        //        }
 
+        length1 = bone1.transform.localScale.y;
+        length2 = bone2.transform.localScale.y;
+        chainLength = 5;
     }
 
     // Update is called once per frame
@@ -85,9 +116,14 @@ public class IkSolver : MonoBehaviour
         //we dont have a valid solution
         effectorDistance = Vector3.Distance(baseEffector.transform.position, endEffector.transform.position);
         Debug.Log(effectorDistance);
-
+        Debug.Log(chainLength);
+        constrainPosition = poleVector.position;
+        basePosition = baseEffector.position;
+        endPosition = endEffector.position;
         if( effectorDistance < chainLength)
         {
+            //=============================================
+            //solve for positions!
             //Step1
             //calculate constraint displacement
             constraintDisplacement = CalculateDisplacement(constrainPosition, basePosition);
@@ -104,8 +140,23 @@ public class IkSolver : MonoBehaviour
             //find cross product of normal plane and effector displacement
             normalizedHeight = CalculateCrossProductOfVector3(normalizedNPlane, normalizedDisplacement);
 
-            
+            //Step3
+            //calculate herons formula
+            HeronsFormula();
 
+            //Step 4
+            //calculate pythagorean theorem
+            distance = PythagoreanTheorem(length1, height);
+
+            //Step 5
+            //calculate final location
+            finalPosition = basePosition + (distance * normalizedDisplacement) + (height * normalizedHeight);
+
+            tochange.position = finalPosition;
+            //===========================================
+            //Solve for Orientations!
+
+            //Debug.Log(finalPosition.x + " " + finalPosition.y + " " + finalPosition.z);
 
 
         }
